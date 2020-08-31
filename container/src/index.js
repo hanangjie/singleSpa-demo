fcc247368ad7f83b4ea4085fd6947e9790b50ec8
hanangjie <hanangjie@gmail.com>
2020-08-31 15:39:48 +0800

    feat: 内容整理

--- container/src/index.js
+++ container/src/index.js
@@ -1,12 +1,5 @@
 import * as singleSpa from "single-spa";
-// const name = "app";
-// const app = () => import("./pages/App/index.js");
-// const activeWhen = "/app";
-// singleSpa.registerApplication({
-//   name,
-//   app,
-//   activeWhen,
-// });
+
 const name = "demo";
 const app = () => System.import("@react-sp/demo");
 app().then(e => {
